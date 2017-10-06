03a7e12e5da5ba8c54ae8d04dd7535fc98471f9f
brett <blarney@myseneca.ca>
2017-10-06 14:35:25 -0400

    first

---
+++ Part3.java
@@ -0,0 +1,66 @@
+package comp2402a1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Part3 {
+
+	/**
+	 * Your code goes here - see Part0 for an example
+	 * @param r the reader to read from
+	 * @param w the writer to write to
+	 * @throws IOException
+	 */
+	public static void doIt(BufferedReader r, PrintWriter w) throws IOException {
+		// Your code goes here - see Part0 for an example
+		List<String> a = new LinkedList<String>();
+		String line;
+		while ((line = r.readLine()) != null) {
+			a.add(line);
+		}
+		Collections.sort(a);
+		Iterator<String> i = a.iterator();
+		while (i.hasNext()) {
+			w.println(i.next());
+		}
+	}
+
+
+	/**
+	 * The driver.  Open a BufferedReader and a PrintWriter, either from System.in
+	 * and System.out or from filenames specified on the command line, then call doIt.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			BufferedReader r;
+			PrintWriter w;
+			if (args.length == 0) {
+				r = new BufferedReader(new InputStreamReader(System.in));
+				w = new PrintWriter(System.out);
+			} else if (args.length == 1) {
+				r = new BufferedReader(new FileReader(args[0]));
+				w = new PrintWriter(System.out);
+			} else {
+				r = new BufferedReader(new FileReader(args[0]));
+				w = new PrintWriter(new FileWriter(args[1]));
+			}
+			long start = System.nanoTime();
+			doIt(r, w);
+			w.flush();
+			long stop = System.nanoTime();
+			System.out.println("Execution time: " + 10e-9 * (stop-start));
+		} catch (IOException e) {
+			System.err.println(e);
+			System.exit(-1);
+		}
+	}
+}
